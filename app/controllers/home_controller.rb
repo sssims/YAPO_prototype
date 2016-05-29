a4c59df685eb27a6b07a805e7817c5d2d182d620
Shane <shanessims@gmail.com>
2016-05-29 01:50:15 -0700

    More work on calendar. Also a dummy journal entry and planner display added to home page view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,16 @@
+require 'date'
+
 class HomeController < ApplicationController
+
+  def index
+
+    today = Date.today
+
+    @month = today.strftime("%B")
+    @month_start_day = Date.new(today.year, today.month, 1).wday
+    @days_in_month   = Date.new(today.year, today.month, -1).day 
+    
+
+  end
+
 end
